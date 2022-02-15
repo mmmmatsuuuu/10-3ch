676cb99a4992132579b2c05d6c60de7ac14d0ce1
mmmmatsuuuu <matsumura-t-mi@g.bears.ed.jp>
2022-02-15 22:40:29 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.GITHUB_ACTIONS ? "/next-pages-test" : "",
+  basePath: process.env.GITHUB_ACTIONS ? "/10-3ch" : "",
   trailingSlash: true,
   reactStrictMode: true,
 }
