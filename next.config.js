b272ea51550fe9fb12de08a54fb68865dd1f4434
mmmmatsuuuu <matsumura-t-mi@g.bears.ed.jp>
2022-02-23 10:47:08 +0900

    update config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.GITHUB_ACTIONS ? process.env.BASE_PATH : '',
-  basePath: process.env.GITHUB_ACTIONS ? process.env.BASE_PATH : '',
+  assetPrefix: process.env.GITHUB_ACTIONS ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+  basePath: process.env.GITHUB_ACTIONS ? process.env.NEXT_PUBLIC_BASE_PATH : '',
   trailingSlash: true,
   reactStrictMode: true,
 }
